bcd74104ab54f7fd8d7d05d62f71b3ad7e216db0
SiMeuDa <yeskwon05@dgu.ac.kr>
2026-08-08 06:45:40 +0000

    fix: erase user input

--- classical/Caesar/test/unit_test.c
+++ classical/Caesar/test/unit_test.c
@@ -4,112 +4,18 @@
 #include <string.h>
 #define BUFFER_SIZE 1024
 
-#ifdef MULTI_TEST
 typedef struct{
 	char str[128];
 	int key;
 }CASE_TEST;
 
-#endif
 
 int main(int argc, char* argv[])
 {
-	char buffer[BUFFER_SIZE] = {'\0'}, input_text[8 * BUFFER_SIZE] = {'\0'};
-	char* output_text;
-	int key, isEncrypt = 1;
-        size_t	buf_len = 0, in_len = 0;
+	int key;
 	FILE* fp;
 	void* ret_ptr = NULL;
 
-#ifndef MULTI_TEST
-	/*
-	 * Set FILE ptr according to argument value
-	 */
-	if(argc == 1){
-		printf("[Input Text]: ");
-		fp = stdin;
-	}else if(strncmp("-f", argv[1], 3) == 0){
-		fp = fopen("./test_input.txt", "r");
-		if(fp == NULL){
-			fprintf(stderr, "[ERROR]: FAILED TO OPEN FILE\n");
-			return CAESAR_INVALID_FILE;
-	}}
-	
-	/*
-	 * Take Text from file ptr
-	 */
-	do{
-		ret_ptr = fgets(buffer, BUFFER_SIZE, fp);
-		if(ret_ptr == NULL){
-			break;
-		}
-
-		in_len = strlen(input_text);
-		buf_len = strlen(buffer);
-
-		if(sizeof(input_text) - in_len > buf_len){
-			strncat(input_text, buffer, buf_len);
-		}else{
-			strncat(input_text, buffer, in_len);
-		}
-	}while(fp != stdin);
-
-	/*
-	 * Take Key and isEncrypt from User
-	 * with checking validation
-	 */
-	printf("[Input Key]: ");
-	if(!scanf("%d", &key)){
-		fprintf(stderr, "[ERROR]: INVALID INPUT\n");
-		return CAESAR_INVALID_INPUT;
-	}
-	
-	printf("[Ecrypt(yes: 1, no: 0)]: ");
-	if(!scanf("%d", &isEncrypt)){
-		fprintf(stderr, "[ERROR]: INVALID INPUT\n");
-		return CAESAR_INVALID_INPUT;
-	}
-
-	
-	output_text = (char*)malloc(sizeof(char) * (strlen(input_text) + 1));
-	if(output_text == NULL){
-		fprintf(stderr, "[ERROR]: FAILED TO ALLOCATE MEMORY\n");
-		return CAESAR_NULL_POINTER;
-	}
-	
-	/*
-	 * Encryption Logic
-	 */
-	if(isEncrypt == 1){
-		if(caesar_encrypt(
-			input_text, 
-			output_text, 
-			&key
-		) != CAESAR_SUCCESS){
-			free(output_text);
-			fprintf(stderr, "[ERROR]: FAILED TO ENCRYPT MESSAGE\n");
-			return CAESAR_INVALID_INPUT;
-	}}else if(isEncrypt == 0){
-		if(caesar_decrypt(
-			input_text,
-			output_text,
-			&key
-		) != CAESAR_SUCCESS){
-			free(output_text);
-			fprintf(stderr, "[ERROR]: FAEILD TO DECRYPT MESSAGE\n");
-			return CAESAR_INVALID_INPUT;
-	}}else{
-		free(output_text);
-		fprintf(stderr, "[ERROR]: INVALID INPUT\n");
-		return CAESAR_INVALID_INPUT;
-	}
-
-	printf("[Output]: %s\n", output_text);
-
-	free(output_text);
-#endif
-
-#ifdef MULTI_TEST
 	/** Expected Checking List
 	 * 1. Basic En/Decryption
 	 * 2. Boundary Value
@@ -185,6 +91,5 @@ int main(int argc, char* argv[])
 
 	}
 	
-#endif
 	return CAESAR_SUCCESS;
 }
